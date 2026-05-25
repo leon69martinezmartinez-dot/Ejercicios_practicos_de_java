b20547012744c1ca1b70a79bf2e84e12d9501cf5
leon_martinez <leon69martinezmartinez@gmail.com>
2026-05-24 20:01:44 -0500

    actualizacion

--- miniencuesta_sobre_uso_de_internet.java
+++ miniencuesta_sobre_uso_de_internet.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 public class miniencuesta_sobre_uso_de_internet {
     public static void main(String[] args) {
+        // se inicia el Scanner
         try(Scanner sc=new Scanner(System.in)){
+            //variables
             int personas=0;
             int mas_de_4_horas=0;
             float horas_de_uso=0;
             float prom;
-            while (true) { 
+            //bucle
+            while (true) {
+                // encuesta
                 System.out.println("cuantas horas usa el intenet al dia?");
                 float uso=sc.nextFloat();
                 horas_de_uso+=uso;
@@ -14,8 +18,10 @@ public class miniencuesta_sobre_uso_de_internet {
                     mas_de_4_horas++;
                 }
                 personas++;
+                //terminar encuesta
                 System.out.println("¿Terminar encuesta? (si/no)");
                 String confirmacion = sc.next();
+                //validacion para terminar encuesta
                 if (confirmacion.equals("si")){
                     prom = horas_de_uso / personas;
                     System.out.println("Personas encuestadas: " + personas);
@@ -24,8 +30,10 @@ public class miniencuesta_sobre_uso_de_internet {
                     break;
                 }
             }
+            //cierre de Scanner
         sc.close();
         }
+        //si ocurre un error arroja esto
         catch(Exception e){
             System.out.println("ha ocurrido un error");
         }
